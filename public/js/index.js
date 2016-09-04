c07de1174adda13111baaccf0b20e6447278264b
sfxdev <sfx.coding@gmail.com>
2016-09-04 10:57:35 -0500

    Rewrote index.hbs using materialize. Added all materialize dependencies.

--- public/js/index.js
+++ public/js/index.js
@@ -7,4 +7,28 @@
             storageBucket: "notes-5ca5c.appspot.com",
         };
         firebase.initializeApp(config);
+        var uiConfig = {
+        'signInSuccessUrl': '<url-to-redirect-to-on-success>',
+        'signInOptions': [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        'tosUrl': '<your-tos-url>',
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
 })();
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+    
+});
